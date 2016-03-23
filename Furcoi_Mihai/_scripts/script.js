1f9d3dfeb27c144a4a9925f054f65d322a97253b
Mitzy Mtz <Mitzy Mtz>
2016-03-23 20:25:33 +0200

    Proiect 1 v4

--- Furcoi_Mihai/_scripts/script.js
+++ Furcoi_Mihai/_scripts/script.js
@@ -1,11 +1,38 @@
-$(document).ready(function(){
+(function ($, win) {
+    $.fn.inViewport = function (cb) {
+        return this.each(function (i, el) {
+            function visPx() {
+                var H = $(this).height() - 150,
+                    r = el.getBoundingClientRect(),
+                    t = r.top,
+                    b = r.bottom;
+                return cb.call(el, Math.max(0, t > 0 ? H - t : (b < H ? b : H)));
+       } visPx();
+       $(win).on("resize scroll", visPx);
+     });
+  };
+}(jQuery, window));
+
+$("#articole").inViewport(function(px){
+    if(px) $(this).addClass("animateLeft") ;
+});
+
+$("#imagini").inViewport(function(px){
+    if(px) $(this).addClass("animateRight") ;
+});
+
+$("#videoclipuri").inViewport(function(px){
+    if(px) $(this).addClass("animateLeft") ;
+});
+
+/*$(document).ready(function(){
     $(window).scroll(function(event){
         var y = $(this).scrollTop(); 
         if (y>=250){
             $('#imagini').addClass('animateRight');
         }   
-        if (y>=550){
+        if (y>=450){
             $('#videoclipuri').addClass('animateLeft');
         }
     }); 
-});
+});*/
